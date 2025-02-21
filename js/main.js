2d6571b45bb9c3f8c61d9326b9053445b1129b16
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-02-21 16:09:15 +0200

    add styles for the feedback section, connect the feedback-slider

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ const menuMobile = document.querySelector('.header-menu-list');
 menuBtn.addEventListener('click', () => {
     menuMobile.classList.toggle('menu-open');
 })
+
+const swiperOne = new Swiper('.feedback-slider', {
+
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  }
+
+});
