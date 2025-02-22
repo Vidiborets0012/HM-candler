4c4daac781abcfcbfef4e2047f58f972709d1209
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-02-22 19:16:46 +0200

    add styles for the questions section

--- js/main.js
+++ js/main.js
@@ -36,3 +36,25 @@ const swiperTwo = new Swiper('.certificates-slider', {
         }
     },
 });
+
+const accordeon = document.querySelector('.accordeon');
+const accordeonTitles = accordeon.querySelectorAll('.accordeon-title');
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+    accordeonTitle.addEventListener('click', function () {
+        
+        const currentText = accordeonTitle.parentElement.querySelector('.accordeon-text');
+
+        accordeonTitle.classList.toggle('accordeon-title-active');
+
+        currentText.classList.toggle('accordeon-text-visible');
+
+        if (currentText.classList.contains('accordeon-text-visible')) {
+            currentText.style.maxHeight = currentText.scrollHeight + 'px'
+
+        } else {
+            currentText.style.maxHeight = null;
+
+        }
+    })
+})
